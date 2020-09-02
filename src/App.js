1dd99ef091d7070ff706d81463d417fb98660e5a
songkg7 <56438906+songkg7@users.noreply.github.com>
2020-09-02 23:31:22 +0900

    Apply Material UI

--- src/App.js
+++ src/App.js
@@ -2,10 +2,30 @@ import React, { Component } from "react";
 import Customer from "./components/Customer";
 import "./App.css";
 
+import Paper from "@material-ui/core/Paper";
+
+import Table from "@material-ui/core/Table";
+import TableHead from "@material-ui/core/TableHead";
+import TableBody from "@material-ui/core/TableBody";
+import TableRow from "@material-ui/core/TableRow";
+import TableCell from "@material-ui/core/TableCell";
+
+import { withStyles } from "@material-ui/core/styles";
+
+const styles = {
+  root: {
+    width: "100%",
+    overflowX: "auto",
+  },
+  table: {
+    minWidth: 1080,
+  },
+};
+
 const customers = [
   {
     id: 1,
-    image: "http://placeimg.com/64/64/1",
+    image: "http://placeimg.com/64/64/1", // 아무 이미지나 랜덤으로 가져와주는 사이트 .../size/size/number
     name: "Selly",
     birthday: "951222",
     gender: "female",
@@ -31,24 +51,39 @@ const customers = [
 
 class App extends Component {
   render() {
+    const { classes } = this.props;
     return (
-      <div>
-        {customers.map((c) => {
-          return (
-            <Customer
-              key={c.id} // map을 사용할 때는 key를 지정해줘야 오류가 발생하지 않음
-              id={c.id}
-              image={c.image}
-              name={c.name}
-              birthday={c.birthday}
-              gender={c.gender}
-              job={c.job}
-            />
-          );
-        })}
-      </div>
+      <Paper className={classes.root}>
+        <Table className={classes.table}>
+          <TableHead>
+            <TableRow>
+              <TableCell>번호</TableCell>
+              <TableCell>이미지</TableCell>
+              <TableCell>이름</TableCell>
+              <TableCell>생년월일</TableCell>
+              <TableCell>성별</TableCell>
+              <TableCell>직업</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {customers.map((c) => {
+              return (
+                <Customer
+                  key={c.id} // map을 사용할 때는 key를 지정해줘야 오류가 발생하지 않음
+                  id={c.id}
+                  image={c.image}
+                  name={c.name}
+                  birthday={c.birthday}
+                  gender={c.gender}
+                  job={c.job}
+                />
+              );
+            })}
+          </TableBody>
+        </Table>
+      </Paper>
     );
   }
 }
 
-export default App;
+export default withStyles(styles)(App);
