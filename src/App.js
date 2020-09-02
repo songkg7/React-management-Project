d882d25b4eda283e8126ed8068c1646ab000e091
songkg7 <56438906+songkg7@users.noreply.github.com>
2020-09-02 16:22:22 +0900

    Create Customer.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,54 @@
-import React from "react";
-import logo from "./logo.svg";
+import React, { Component } from "react";
+import Customer from "./components/Customer";
 import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const customers = [
+  {
+    id: 1,
+    image: "http://placeimg.com/64/64/1",
+    name: "Selly",
+    birthday: "951222",
+    gender: "female",
+    job: "programmer",
+  },
+  {
+    id: 2,
+    image: "http://placeimg.com/64/64/2",
+    name: "홍길동",
+    birthday: "961222",
+    gender: "male",
+    job: "student",
+  },
+  {
+    id: 3,
+    image: "http://placeimg.com/64/64/3",
+    name: "아영",
+    birthday: "940301",
+    gender: "female",
+    job: "Composer",
+  },
+];
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        {customers.map((c) => {
+          return (
+            <Customer
+              key={c.id} // map을 사용할 때는 key를 지정해줘야 오류가 발생하지 않음
+              id={c.id}
+              image={c.image}
+              name={c.name}
+              birthday={c.birthday}
+              gender={c.gender}
+              job={c.job}
+            />
+          );
+        })}
+      </div>
+    );
+  }
 }
 
 export default App;
