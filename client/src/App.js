b8b5eda4f3738d4352595faaff79a12450ce27b3
songkg7 <56438906+songkg7@users.noreply.github.com>
2020-09-03 03:20:36 +0900

    Add Get Customers REST API Module

--- client/src/App.js
+++ client/src/App.js
@@ -22,34 +22,23 @@ const styles = {
   },
 };
 
-const customers = [
-  {
-    id: 1,
-    image: "http://placeimg.com/64/64/1", // 아무 이미지나 랜덤으로 가져와주는 사이트 .../size/size/number
-    name: "Selly",
-    birthday: "951222",
-    gender: "female",
-    job: "programmer",
-  },
-  {
-    id: 2,
-    image: "http://placeimg.com/64/64/2",
-    name: "홍길동",
-    birthday: "961222",
-    gender: "male",
-    job: "student",
-  },
-  {
-    id: 3,
-    image: "http://placeimg.com/64/64/3",
-    name: "아영",
-    birthday: "940301",
-    gender: "female",
-    job: "Composer",
-  },
-];
-
 class App extends Component {
+  state = {
+    customers: "",
+  };
+
+  componentDidMount() {
+    this.callApi()
+      .then((res) => this.setState({ customers: res }))
+      .catch((err) => console.log(err));
+  }
+
+  callApi = async () => {
+    const response = await fetch("/api/customers");
+    const body = response.json();
+    return body;
+  };
+
   render() {
     const { classes } = this.props;
     return (
@@ -66,19 +55,21 @@ class App extends Component {
             </TableRow>
           </TableHead>
           <TableBody>
-            {customers.map((c) => {
-              return (
-                <Customer
-                  key={c.id} // map을 사용할 때는 key를 지정해줘야 오류가 발생하지 않음
-                  id={c.id}
-                  image={c.image}
-                  name={c.name}
-                  birthday={c.birthday}
-                  gender={c.gender}
-                  job={c.job}
-                />
-              );
-            })}
+            {this.state.customers
+              ? this.state.customers.map((c) => {
+                  return (
+                    <Customer
+                      key={c.id} // map을 사용할 때는 key를 지정해줘야 오류가 발생하지 않음
+                      id={c.id}
+                      image={c.image}
+                      name={c.name}
+                      birthday={c.birthday}
+                      gender={c.gender}
+                      job={c.job}
+                    />
+                  );
+                })
+              : ""}
           </TableBody>
         </Table>
       </Paper>
--- server.js
+++ server.js
@@ -6,8 +6,33 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/api/hello", (req, res) => {
-  res.send({ message: "Hello Express!" });
+app.get("/api/customers", (req, res) => {
+  res.send([
+    {
+      id: 1,
+      image: "http://placeimg.com/64/64/1", // 아무 이미지나 랜덤으로 가져와주는 사이트 .../size/size/number
+      name: "Selly",
+      birthday: "951222",
+      gender: "female",
+      job: "programmer",
+    },
+    {
+      id: 2,
+      image: "http://placeimg.com/64/64/2",
+      name: "홍길동",
+      birthday: "961222",
+      gender: "male",
+      job: "student",
+    },
+    {
+      id: 3,
+      image: "http://placeimg.com/64/64/3",
+      name: "아영",
+      birthday: "940301",
+      gender: "female",
+      job: "Composer",
+    },
+  ]);
 });
 
 app.listen(port, () => console.log(`Listening  on port ${port}`));
