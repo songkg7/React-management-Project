5592c249790db12a21a68cb2646122c04152b1aa
songkg7 <56438906+songkg7@users.noreply.github.com>
2020-09-03 17:24:26 +0900

    Add Customer API loading view

--- client/src/App.js
+++ client/src/App.js
@@ -9,7 +9,7 @@ import TableHead from "@material-ui/core/TableHead";
 import TableBody from "@material-ui/core/TableBody";
 import TableRow from "@material-ui/core/TableRow";
 import TableCell from "@material-ui/core/TableCell";
-
+import CircularProgress from "@material-ui/core/CircularProgress";
 import { withStyles } from "@material-ui/core/styles";
 
 const styles = {
@@ -20,14 +20,38 @@ const styles = {
   table: {
     minWidth: 1080,
   },
+  progress: {
+    margin: "10px",
+  },
 };
+/*
+
+리액트 라이프 사이클
+
+1) constructor()
+
+2) componentWillMount()
+
+3) render()
+
+4) compornentDidMount()
+
+*/
+
+/*
+
+props or state => shouldComponentUpdate()
+
+*/
 
 class App extends Component {
   state = {
     customers: "",
+    completed: 0,
   };
 
   componentDidMount() {
+    this.timer = setInterval(this.progress, 20);
     this.callApi()
       .then((res) => this.setState({ customers: res }))
       .catch((err) => console.log(err));
@@ -37,6 +61,11 @@ class App extends Component {
     const response = await fetch("/api/customers");
     const body = response.json();
     return body;
+  };
+
+  progress = () => {
+    const { completed } = this.state;
+    this.setState({ completed: completed >= 100 ? 0 : completed + 1 });
   };
 
   render() {
@@ -55,21 +84,31 @@ class App extends Component {
             </TableRow>
           </TableHead>
           <TableBody>
-            {this.state.customers
-              ? this.state.customers.map((c) => {
-                  return (
-                    <Customer
-                      key={c.id} // map을 사용할 때는 key를 지정해줘야 오류가 발생하지 않음
-                      id={c.id}
-                      image={c.image}
-                      name={c.name}
-                      birthday={c.birthday}
-                      gender={c.gender}
-                      job={c.job}
-                    />
-                  );
-                })
-              : ""}
+            {this.state.customers ? (
+              this.state.customers.map((c) => {
+                return (
+                  <Customer
+                    key={c.id} // map을 사용할 때는 key를 지정해줘야 오류가 발생하지 않음
+                    id={c.id}
+                    image={c.image}
+                    name={c.name}
+                    birthday={c.birthday}
+                    gender={c.gender}
+                    job={c.job}
+                  />
+                );
+              })
+            ) : (
+              <TableRow>
+                <TableCell colSpan="6" align="center">
+                  <CircularProgress
+                    className={classes.progress}
+                    variant="determinate"
+                    value={this.state.completed}
+                  />
+                </TableCell>
+              </TableRow>
+            )}
           </TableBody>
         </Table>
       </Paper>
